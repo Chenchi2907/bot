0f15802e408b7ead4c8e96f41151de42d7d66dc6
Chenchi2907 <105590093+Chenchi2907@users.noreply.github.com>
2022-10-31 20:34:39 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,33 +1,47 @@
+from typing_extensions import Self
 import discord
 from discord.ext import commands
-import json
-with open('setting.json',mode='r',encoding="utf-8") as jfile:
-    jdata=json.load(jfile)
+
+
+
+
+
 #try add this 
-intents=intents=discord.Intents.all()
+intents=discord.Intents.all()
+
 
 #if the above don't work, try with this
 #intents = discord.Intents()
 #intents.members = True
 
-TOKEN = 'MTAwODk0NzUzMzIzNTYxNzk0Mw.GWyHXz.3GcrNdobFdZYCUBm_5xfwdZhBA6HXNyDZKfD7o'
-bot=commands.Bot(command_prefix='!',intents=intents)
 
-#Events
-
+bot=commands.Bot(command_prefix='!',intents=intents,help_command=None)
 @bot.event
-async def  on_member_join(member):
-    channel=bot.get_channel(int(jdata['Welcom_channal']))
-    await channel.send(f'{member} join!' )
+async def on_ready():
+    print("Bot is Ready")
+
+@bot.command(name="help")
+async def help(ctx):
+    embed=discord.Embed(
+        title='help',
+        description="Help for you",
+        color=discord.color.red()
+    
+    
+    )
+    await ctx.send(embed=embed)
+    
 
 
 
-@bot.event
-async def  on_member_remove(member):
-    channel=bot.get_channel(int(jdata['leave_channal']))
-    await channel.send(f'{member} left!' )
-  
-@bot.command()
-async def test(ctx):
-    await ctx.send(f'{round(bot.latency*1000)} (ms)')
-bot.run(jdata["TOKEN"])
+
+
+
+
+
+bot.run('MTAwODk0NzUzMzIzNTYxNzk0Mw.GGwVdq.Aj53FsyS3keF6Db43QaISpm9yV7pjhaBW7khR4')
+
+
+
+
+
