f8f9300e1dc02f08cad5c1a8c801da725c68f824
Chenchi2907 <105590093+Chenchi2907@users.noreply.github.com>
2022-12-05 20:40:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-jedj
+import discord
+from discord.ext import commands
+from discord import activity
+
+class Main(commands.Cog):
+    def __init__(self,bot):
+        self.bot=bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.bot.change_presence(activity=discord.Game('im online'))
+        print('is online')
+    @commands.command()
+    async def ping(self,ctx):
+        await ctx.send(f"{round(self.bot.latency*1000)} (ms)")
+
+    
