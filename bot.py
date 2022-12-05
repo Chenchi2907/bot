9c4719f7c69837ff87a7b73df55a928026ef99f8
Chenchi2907 <105590093+Chenchi2907@users.noreply.github.com>
2022-12-05 20:40:09 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,44 +1,62 @@
-from typing_extensions import Self
+
 import discord
 from discord.ext import commands
-
-
-
-
-
-#try add this 
+import os
+from main import Main
 intents=discord.Intents.all()
-
-
+intents.members=True
+from datetime import datetime
+import time
+from meme import Main_second
+#try add this 
 #if the above don't work, try with this
 #intents = discord.Intents()
-intents.members = True
-
 
 bot=commands.Bot(command_prefix='!',intents=intents,help_command=None)
-@bot.event
-async def on_ready():
-    print("Bot is Ready")
-
-@bot.command(name="help")
+@bot.command(name='help')
 async def help(ctx):
     embed=discord.Embed(
-        title='help',
-        description="Help for you",
-        color=discord.Color.red()   
+        title='Help',
+        description='Help for you',
+        color=discord.Color.blue()
     )
-        
-    await ctx.send(embed=embed)
     
+    await ctx.send(embed=embed)
+
+@bot.event
+async def on_ready():
+    print("Bot is online")
+    await bot.add_cog(Main(bot))
+    await bot.add_cog(Main_second(bot))
+
+@bot.event
+async def on_member_join(member):
+    channel=bot.get_channel(1034052336596824176)
+    em=discord.Embed(
+        title=f'Welcom',
+        color=discord.Color.random(),
+        timestamp=datetime.utcnow()
+        ).add_field(
+        name=f'<1035844076228059136> Rules',
+        value=f'<#1042775868382134292>'
+        ).add_field(
+        name=f'<1035844076228059136> Chat',
+        value='<#1042776334931345489>'
+        ).add_field(
+        name=f'Total members',
+        value=f'{member.guild.member_count}'
+        ).set_footer(text=f'{member.name} just joined')
+    await channel.send(embed=em)
 
 
 
+async def on_member_remove(member):
+    channel=bot.get_channel(1034052502431219712)
+    await channel.send(f'{member} left!')
 
 
 
-
-
-bot.run('MTAwODk0NzUzMzIzNTYxNzk0Mw.G-xWOB.DNmzLmdGd5SXqN3TrAV36DdzLVRheXoZkUvKQY')
+bot.run('MTAwODk0NzUzMzIzNTYxNzk0Mw.GlrbnO.hUMxMMKd6gz0AyWagwvveNT8hjsX4RgQ9G0iuM')
 
 
 
